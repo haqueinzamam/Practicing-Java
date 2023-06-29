64914c07eda104eb9d2718c3c1b59a1bee07eee1
haqueinzamam <inzamamulhaque7287@gmail.com>
2023-06-30 00:37:10 +0530

    first commit

---
+++ variables.java
@@ -0,0 +1,25 @@
+public class variables {
+    public static void main(String[] args) {
+        //Variable
+        String name = "Inzamam";
+        //Primitive data type
+        int number = 28;
+        byte age = 30;
+        short s = 5;
+        long l = 454545645L;
+        float f = 45465.54564F;
+        char letter = 'S';
+        double d = 1.54654;
+        boolean b = true; //or false
+
+        System.out.println(name);
+        System.out.println(number);
+        System.out.println(age);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(letter);
+        System.out.println(d);
+        System.out.println(b);
+    }
+}
