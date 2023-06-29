a7d77bef48cbb53f4ef5053cffeb2b8347679e8a
haqueinzamam <inzamamulhaque7287@gmail.com>
2023-06-30 00:49:34 +0530

    add strings.java

---
+++ strings.java
@@ -0,0 +1,10 @@
+public class strings {
+    public static void main(String[] args) {
+        //concatetnate 
+        String name1 = "Zakariya";
+        String name2 = "Inzamam";
+        String name3 = name1 + " and " + name2;
+        System.out.println(name3);
+    }
+}
+ 
