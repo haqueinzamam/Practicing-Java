598c4408bac4306afce64ca317876a2ae8d441c2
haqueinzamam <inzamamulhaque7287@gmail.com>
2023-06-30 15:46:31 +0530

    arrays

---
+++ arrays.java
@@ -0,0 +1,10 @@
+public class arrays {
+    public static void main(String[] args) {
+        int[] value = new int[3];
+        value[0] = 98;
+        value[1] = 89;
+        value[2] = 67;
+        System.out.println(value[2]);
+
+    }
+}
